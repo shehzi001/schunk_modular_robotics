c94a2bc73e23093b7ec5f5c23eb71caa41c4dc3b
Shehzad Ahmed <shehzad@c069-workstation-1.(none)>
2014-04-28 20:53:13 +0200

    added schunk_sdh_control_interface

---
+++ schunk_sdh_control_interface/build/CMakeFiles/schunk_sdh_control_interface.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/schunk_sdh_control_interface.dir/cpp/src/schunk_sdh_control_interface.cpp.o"
+  "schunk_sdh_control_interface.pdb"
+  "schunk_sdh_control_interface"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/schunk_sdh_control_interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ schunk_sdh_control_interface/cpp/src/schunk_sdh_control_interface.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <schunk_sdh/sdh.h>
+
+
+using namespace std;
+
+
+
+class SchunkSdhControlInterface
+{
+
+private:
+	SDH::cSDH *sdh_;
+	std::vector<SDH::cSDH::eAxisState> state_;
+	std::string sdhdevicetype_;
+	std::string sdhdevicestring_;
+	int sdhdevicenum_;
+	int baudrate_, id_read_, id_write_;
+	double timeout_;
+
+
+	bool isInitialized_;
+	bool isDSAInitialized_;
+	bool isError_;
+	int DOF_;
+	double pi_;
+
+	std::vector<std::string> joint_names_;
+	std::vector<int> axes_;
+	std::vector<double> targetAngles_; // in degrees
+	std::vector<double> velocities_; // in rad/s
+	bool hasNewGoal_;
+	std::string operationMode_; 
+
+public:
+	SchunkSdhControlInterface()
+	{
+		pi_ = 3.1415926;
+		isError_ = false;
+	}
+
+	~SchunkSdhControlInterface()
+	{
+		if(isInitialized_)
+				sdh_->Close();
+			delete sdh_;
+	}
+
+	bool init()
+		{
+			// initialize member variables
+			isInitialized_ = false;
+			isDSAInitialized_ = false;
+			hasNewGoal_ = false;
+
+			// pointer to sdh
+			sdh_ = new SDH::cSDH(false, false, 0); //(_use_radians=false, bool _use_fahrenheit=false, int _debug_level=0)
+
+			// setting hardware parameters
+			sdhdevicetype_ = std::string("PCAN");
+			sdhdevicestring_ = std::string("/dev/pcan0");
+			sdhdevicenum_ = 0;
+			baudrate_ = 1000000;
+			timeout_ = (double)0.04;
+			id_read_ = 43;
+			id_write_ = 42;
+			DOF_ = 7;
+			
+			// define axes to send to sdh
+			axes_.resize(DOF_);
+
+			for(int i = 0; i < DOF_; i++)
+			{
+				axes_[i] = i;
+			}
+			
+			state_.resize(axes_.size());
+
+			operationMode_ = std::string("position");
+
+			return true;
+		}
+
+
+};
+
+int main(int argc, char** argv)
+{
+    
+    cout << "schunk sdh control interface running" << endl;
+
+    SchunkSdhControlInterface sdh_interface;
+    
+    if (!sdh_interface.init()) return 0;
+
+    return 0;
+}
